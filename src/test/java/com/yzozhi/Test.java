5689e3cbd46497d1ba6b4fd8b12df77bc00415b8
wsy741963 <wsy741963@live.com>
2023-05-31 00:08:28 +0800

    test

--- src/test/java/com/yzozhi/Test.java
+++ src/test/java/com/yzozhi/Test.java
@@ -8,7 +8,7 @@ public class Test {
     public void file() {
         String s = Regex.regex("national_total", "(\\w+)_(\\w+)", 2);
         log.info("Success:" + s);
-        // FileHandle.fileRename("2017");
+        FileHandle.fileRename("2017");
         System.out.println("你好");
         log.info("你好");
     }
