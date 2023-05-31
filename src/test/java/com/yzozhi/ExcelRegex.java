061a14b38adddc4eb3c969e71691419023ebc865
wsy741963 <wsy741963@live.com>
2023-05-31 15:26:02 +0800

    regex regular

--- src/test/java/com/yzozhi/ExcelRegex.java
+++ src/test/java/com/yzozhi/ExcelRegex.java
@@ -1,6 +1,9 @@
 package com.yzozhi;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.Test;
 
 import com.alibaba.excel.EasyExcelFactory;
 
@@ -8,12 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ExcelRegex {
+    @Test
     public static void main(String[] args) {
-        // 定义文件
-        String fileRead = "2017.xlsx";
-        String fileResult = "result.xlsx";
-
         long start = System.currentTimeMillis();
+
+        // 定义文件
+        String fileRead = "2020_result.xlsx";
+        String fileResult = "2020_regex.xlsx";
+
         // 读取表格
         DataListener datasListener = new DataListener();
         EasyExcelFactory.read(fileRead, Data.class, datasListener)
@@ -21,81 +26,47 @@ public class ExcelRegex {
                 .doRead();
 
         List<Data> datas = datasListener.getDatas();
-        // 校正原数据，不需要创建新对象
+        List<Data> newDatas = new ArrayList<>();
         int count = 0;
         for (Data data : datas) {
-            if (data.getNumDeath() == null) {
-                // 处理发病数据
-                data.setNumIncidence(reFilter(data.getNumIncidence()));
-                data.setFreqIncidence(reFilter(data.getFreqIncidence()));
-                data.setAge0(reFilter(data.getAge0()));
-                data.setAge1(reFilter(data.getAge1()));
-                data.setAge5(reFilter(data.getAge5()));
-                data.setAge10(reFilter(data.getAge10()));
-                data.setAge15(reFilter(data.getAge15()));
-                data.setAge20(reFilter(data.getAge20()));
-                data.setAge25(reFilter(data.getAge25()));
-                data.setAge30(reFilter(data.getAge30()));
-                data.setAge35(reFilter(data.getAge35()));
-                data.setAge40(reFilter(data.getAge40()));
-                data.setAge45(reFilter(data.getAge45()));
-                data.setAge50(reFilter(data.getAge50()));
-                data.setAge55(reFilter(data.getAge55()));
-                data.setAge60(reFilter(data.getAge60()));
-                data.setAge65(reFilter(data.getAge65()));
-                data.setAge70(reFilter(data.getAge70()));
-                data.setAge75(reFilter(data.getAge75()));
-                data.setAge80(reFilter(data.getAge80()));
-                data.setAge85(reFilter(data.getAge85()));
-                data.setRateCrude(reFilter(data.getRateCrude()));
-                data.setAsrChinaIncidence(reFilter(data.getAsrChinaIncidence()));
-                data.setAsrWorldIncidence(reFilter(data.getAsrWorldIncidence()));
-                data.setCumRate64Incidence(reFilter(data.getCumRate64Incidence()));
-                data.setCumRate74Incidence(reFilter(data.getCumRate74Incidence()));
-            } else {
-                // 处理死亡的数据
-                data.setNumDeath(reFilter(data.getNumDeath()));
-                data.setFreqDeath(reFilter(data.getFreqDeath()));
-                data.setAge0(reFilter(data.getAge0()));
-                data.setAge1(reFilter(data.getAge1()));
-                data.setAge5(reFilter(data.getAge5()));
-                data.setAge10(reFilter(data.getAge10()));
-                data.setAge15(reFilter(data.getAge15()));
-                data.setAge20(reFilter(data.getAge20()));
-                data.setAge25(reFilter(data.getAge25()));
-                data.setAge30(reFilter(data.getAge30()));
-                data.setAge35(reFilter(data.getAge35()));
-                data.setAge40(reFilter(data.getAge40()));
-                data.setAge45(reFilter(data.getAge45()));
-                data.setAge50(reFilter(data.getAge50()));
-                data.setAge55(reFilter(data.getAge55()));
-                data.setAge60(reFilter(data.getAge60()));
-                data.setAge65(reFilter(data.getAge65()));
-                data.setAge70(reFilter(data.getAge70()));
-                data.setAge75(reFilter(data.getAge75()));
-                data.setAge80(reFilter(data.getAge80()));
-                data.setAge85(reFilter(data.getAge85()));
-                data.setRateCrude(reFilter(data.getRateCrude()));
-                data.setAsrChinaDeath(reFilter(data.getAsrChinaDeath()));
-                data.setAsrWorldDeath(reFilter(data.getAsrWorldDeath()));
-                data.setCumRate64Death(reFilter(data.getCumRate64Death()));
-                data.setCumRate74Death(reFilter(data.getCumRate74Death()));
-            }
-            count++;
-            log.info("校对了第" + count + "条数据");
+            // 校正原数据，不需要创建新对象
+            data.setRateIncidence(reFilter(data.getRateIncidence()));
+            data.setRateDeath(reFilter(data.getRateDeath()));
+            data.setNumIncidence(reFilter(data.getNumIncidence()));
+            data.setFreqIncidence(reFilter(data.getFreqIncidence()));
+            data.setAsrChinaIncidence(reFilter(data.getAsrChinaIncidence()));
+            data.setAsrWorldIncidence(reFilter(data.getAsrWorldIncidence()));
+            data.setCumRate64Incidence(reFilter(data.getCumRate64Incidence()));
+            data.setCumRate74Incidence(reFilter(data.getCumRate74Incidence()));
+            data.setNumDeath(reFilter(data.getNumDeath()));
+            data.setFreqDeath(reFilter(data.getFreqDeath()));
+            data.setAsrChinaDeath(reFilter(data.getAsrChinaDeath()));
+            data.setAsrWorldDeath(reFilter(data.getAsrWorldDeath()));
+            data.setCumRate64Death(reFilter(data.getCumRate64Death()));
+            data.setCumRate74Death(reFilter(data.getCumRate74Death()));
+            data.setCoverPopulation(reFilter(data.getCoverPopulation()));
+            data.setMvPercent(reFilter(data.getMvPercent()));
+            data.setDcoPercent(reFilter(data.getDcoPercent()));
+            data.setMIRate(reFilter(data.getMIRate()));
+            data.setChangeForCr(reFilter(data.getChangeForCr()));
+            newDatas.add(data);
         }
+        count++;
+        log.info("校对了第" + count + "条数据");
 
         // 写入主表格
-        EasyExcelFactory.write(fileResult, Data.class)
+        EasyExcelFactory.write(fileResult, NewData.class)
                 // 是否在原有数据上追加写入数据，否则直接清空
                 // .withTemplate(fileWrite)
                 .sheet()
                 // 是否写入表头
                 // .needHead(false)
                 .doWrite(datas);
-        long end = System.currentTimeMillis();
-        log.warn("\n================================\n写入完成,共" + datas.size()
-                + "条数据\n耗时:" + (end - start) / 1000 + "s\n================================");
+
+        long end = System
+                .currentTimeMillis();
+        log.warn("\n================================\n写入完成,共" + datas.size() + "条数据\n耗时:" + (end - start) / 1000
+                + "s\n================================");
     }
 
     public static String reFilter(String str) {
@@ -104,7 +75,7 @@ public class ExcelRegex {
             return str;
         } catch (Exception e) {
             log.error(e.getMessage());
-            return "";
+            return str;
         }
     }
 }
