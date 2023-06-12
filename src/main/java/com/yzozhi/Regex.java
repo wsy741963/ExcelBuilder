fdb5baff1837d4479ee14ed45abb99477a4c864f
wsy741963 <wsy741963@live.com>
2023-06-12 23:49:17 +0800

    version

--- src/main/java/com/yzozhi/Regex.java
+++ src/main/java/com/yzozhi/Regex.java
@@ -3,9 +3,9 @@ package com.yzozhi;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import lombok.extern.slf4j.Slf4j;
+// import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+// @Slf4j
 public class Regex {
     // 输入参数:需匹配原字符串，正则表达式用括号分组，分组参数
     public static String regex(String str, String regex, int group) {
